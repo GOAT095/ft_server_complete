7fd962d86b22a0c980b55f672ed9d7fc205a9bfc
Abdelhakim Nassif <anassif@e1r13p9.1337.ma>
2019-12-01 16:26:18 +0100

    1-12

--- server/srcs/services-user.sh
+++ server/srcs/services-user.sh
@@ -1,4 +1,4 @@
-!#bin/bash
+#!/bin/bash
 #start services
 service nginx start
 service php7.3-fpm start
@@ -7,9 +7,11 @@ chown -R mysql:mysql /var/lib/mysql
 service mysql start
 #create a user and grant him privileges
 mysql -u root -e "CREATE DATABASE wordpress"
-mysql -u root -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin'"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost'"
+mysql -u root -e "CREATE DATABASE phpmyadmin"
+mysql -u root -e "CREATE USER 'pma'@'localhost' IDENTIFIED BY 'pmapass'"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'pma'@'localhost'"
 mysql -u root -e "FLUSH PRIVILEGES"
-mysql -u admin -padmin wordpress < /var/wordpress.sql
+mysql -u pma -ppmapass wordpress < /var/wordpress.sql
+mysql -u pma -ppmapass phpmyadmin < /var/www/html/phpmyadmin/sql/create_tables.sql
 #to get shell
 /bin/bash
